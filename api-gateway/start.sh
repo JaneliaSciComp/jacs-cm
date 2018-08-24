179e05c37e5492d7b83260f72364e89a6af02a5b
Konrad Rokicki <rokickik@janelia.hhmi.org>
2018-08-24 09:58:11 -0400

    tweak env values

--- api-gateway/start.sh
+++ api-gateway/start.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
-if [[ -z $JWT_SECRET ]]; then
+#!/bin/bash
+if [[ $JWT_SECRET ]]; then
+    echo "Using JWT_SECRET from environment"
+elif [[ -e /app/jwt_secret ]]; then
     export JWT_SECRET=`cat /app/jwt_secret`
     echo "Using JWT_SECRET from /app/jwt_secret"
 else
-    echo "Using JWT_SECRET from environment"
+    echo "You must define a JWT secret either as an env variable (JWT_SECRET) or in a mounted file (/app/jwt_secret)"
 fi
-echo "Using NGINX_SERVERNAME=$NGINX_SERVERNAME"
 /usr/bin/openresty -g "daemon off;"
