084d058dd34d8bfd8471c675484132de68803943
Konrad Rokicki <rokickik@janelia.hhmi.org>
2019-02-26 17:53:01 -0500

    moved containers and environments into subdirectory

--- jacs-init/init-filesystem/ipp/env_config.py
+++
@@ -1,8 +0,0 @@
-MONGODB_HOST='mongodb://mongo1:27017,mongo2:27017,mongo3:27017/ipp?replicaSet=rsJacs&authSource=admin'
-MONGODB_DB='ipp'
-MONGODB_USERNAME='lightsheet'
-MONGODB_PASSWORD='password'
-UPLOAD_FOLDER='/tmp/ipp/upload'
-AUTH_SERVICE_URL='https://api-gateway/SCSW/AuthenticationService/v1/authenticate'
-JACS_HOST='https://api-gateway/SCSW/JACS2AsyncServices/v2/'
-
--- jacs-init/init-filesystem/mysql/sql/1-users.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER flyportalAdmin@'%' IDENTIFIED BY 'FeWO4O';
-GRANT SUPER ON *.* TO flyportalAdmin@'%';
-GRANT ALL PRIVILEGES on flyportal.* to flyportalAdmin@'%';
-CREATE USER flyportalApp@'%' IDENTIFIED BY 'FeWO4W';
-GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE, SHOW VIEW on flyportal.* to flyportalApp@'%';
-CREATE USER flyportalRead@'%' IDENTIFIED BY 'flyportalRead';
-GRANT SELECT on flyportal.* to flyportalRead@'%';
