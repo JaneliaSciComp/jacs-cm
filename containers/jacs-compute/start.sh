e7c15908f12f7a85f67b7a628410c13dbd7ec2e3
Cristian Goina <goinac@hhmi.org>
2019-04-11 17:07:49 -0400

    fixed the service locations

--- containers/jacs-compute/start.sh
+++ containers/jacs-compute/start.sh
@@ -3,10 +3,9 @@
 echo "Running jacs-compute container image with service mode: ${SERVICE_MODE}"
 
 if [[ "$SERVICE_MODE" = "async" ]]; then
-    /app/master/bin/jacs2-asyncweb -b 0.0.0.0 -p 8080 -s "${HOSTNAME}"
-
+    /app/async/bin/jacs2-asyncweb -b 0.0.0.0 -p 8080 -s "${HOSTNAME}"
 elif [[ "$SERVICE_MODE" = "sync" ]]; then
-    /app/master/bin/jacs2-syncweb -b 0.0.0.0 -p 8080 -s "${HOSTNAME}"
+    /app/sync/bin/jacs2-syncweb -b 0.0.0.0 -p 8080 -s "${HOSTNAME}"
 else
     echo "Unsupported service mode specified (with SERVICE_MODE environment variable): ${SERVICE_MODE}"
     exit 1
