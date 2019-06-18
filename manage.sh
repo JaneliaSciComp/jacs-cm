2de323d4be23207d93d50edec580683be34acb3c
Cristian Goina <goinac@janelia.hhmi.org>
2019-06-18 17:35:05 -0400

    fixed the method call for reindex

--- manage.sh
+++ manage.sh
@@ -368,7 +368,7 @@ if [[ "$1" == "rebuildSolrIndex" ]]; then
     done
 
     set -x
-    $SUDO $DOCKER run --env-file .env -u $DOCKER_USER --network ${NETWORK_NAME} ${CONTAINER_PREFIX}jacs-sync curl http://jacs-sync:8080/api/rest-v2/data/searchIndex?clearIndex=true -H "Authorization: APIKEY ${apiKey}"
+    $SUDO $DOCKER run --env-file .env -u $DOCKER_USER --network ${NETWORK_NAME} ${CONTAINER_PREFIX}jacs-sync curl -X PUT http://jacs-sync:8080/api/rest-v2/data/searchIndex?clearIndex=true -H "Authorization: APIKEY ${apiKey}"
     set +x
 
     exit 0
