909f22df5385cf1dee7548c043f8d98fd4cd9be6
Cristian Goina <goinac@janelia.hhmi.org>
2021-12-06 16:46:31 -0500

    filebeat running now

--- containers/jacs-init/filesystem/run.sh
+++ containers/jacs-init/filesystem/run.sh
@@ -269,10 +269,10 @@ else
     echo "Verified filebeat config directory: $filebeat_dir"
 fi
 
-filebeat_containers_dir=$config_dir/docker/containers
+filebeat_containers_dir=$config_dir/filebeat/docker/containers
 if [[ ! -e "$filebeat_containers_dir" ]]; then
     echo "Initializing filebeat containers directory: $filebeat_containers_dir"
-    mkdir $DIR/$filebeat_containers_dir
+    mkdir -p $filebeat_containers_dir
 else
     echo "Verified filebeat containers directory: $filebeat_containers_dir"
 fi
