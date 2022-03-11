05c2ffe2c815cf5a9d06fb3b1f1224920d1dd3cb
Cristian Goina <goinac@janelia.hhmi.org>
2022-03-11 10:18:04 -0500

    script to import index

---
+++ import-index.sh
@@ -0,0 +1,16 @@
+indexName=logstash-2021.02.01
+
+curl -H 'Content-Type: application/json' -X POST http://e03u08.int.janelia.org:9200/_reindex -d "{
+    \"source\": {
+	\"remote\": {
+	    \"host\": \"http://c13u08.int.janelia.org:9200\"
+	},
+	\"index\": \"${indexName}\",
+	\"query\": {
+	    \"match_all\": {}
+	}
+    },
+    \"dest\": {
+	\"index\": \"${indexName}\"
+    }
+}"
