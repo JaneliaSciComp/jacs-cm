9bef8a5caa9ae3e9d1dd50ff636f97adf41e1513
Cristian Goina <goinac@hhmi.org>
2023-02-22 10:10:31 -0500

    always use env-file for init-local-filesystem

--- manage.sh
+++ manage.sh
@@ -357,7 +357,8 @@ fi
 if [[ "$1" == "init-local-filesystem" ]]; then
     echo "Initializing local file system..."
     set -x
-    $SUDO $DOCKER run $ENV_PARAM $PLATFORM_PARAM -v ${REDUNDANT_STORAGE}:${REDUNDANT_STORAGE} -v ${NON_REDUNDANT_STORAGE}:${NON_REDUNDANT_STORAGE} -u $DOCKER_USER ${CONTAINER_PREFIX}jacs-init:${JACS_INIT_VERSION} /app/filesystem/run.sh
+    # this always requires env-file so we don't use ENV_FILE_PARAM here
+    $SUDO $DOCKER run --env-file .env $PLATFORM_PARAM -v ${REDUNDANT_STORAGE}:${REDUNDANT_STORAGE} -v ${NON_REDUNDANT_STORAGE}:${NON_REDUNDANT_STORAGE} -u $DOCKER_USER ${CONTAINER_PREFIX}jacs-init:${JACS_INIT_VERSION} /app/filesystem/run.sh
     set +x
     echo ""
     echo "The local filesystem is initialized. You should now edit the template files in $CONFIG_DIR to match your deployment environment."
