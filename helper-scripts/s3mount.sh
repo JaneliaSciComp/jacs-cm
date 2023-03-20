a3571ec4a0b4ddb72a01497d918321b5b622858d
Cristian Goina <goinac@hhmi.org>
2023-03-20 17:17:08 -0400

    bump up max requests from default 20 to 500

--- helper-scripts/s3mount.sh
+++ helper-scripts/s3mount.sh
@@ -1,3 +1,3 @@
 bucketName=$1
 mkdir -p /data/s3/${bucketName}
-s3fs ${bucketName} /data/s3/${bucketName} -o allow_other,uid=$(id -u jacs),gid=$(id -g jacs)
+s3fs ${bucketName} /data/s3/${bucketName} -o allow_other,multireq_max=500,uid=$(id -u jacs),gid=$(id -g jacs)
